b748ba36363e5662a2fffcbf11814fcd4a52b168
Javier <jagi.ccs@gmail.com>
2020-07-30 16:07:28 +0100

    changes at tournament form

--- src/components/tournament/TournamentForm.js
+++ src/components/tournament/TournamentForm.js
@@ -28,22 +28,7 @@ class TournamentForm extends React.Component {
     render() { 
         return ( 
             <div>
-                {/* <form onSubmit={this.handleSubmit}>
-                    <label>Name:</label>
-                    <input type="text" required name="name" onChange={this.handleChange}/><br/>
-                    <label>Country:</label>
-                    <input type="text" required name="country" onChange={this.handleChange}/><br/>
-                    <label>City:</label>
-                    <input type="text" required name="city" onChange={this.handleChange}/><br/>
-                    <label>Type:</label>
-                    <select value={this.state.types} defaultValue={this.state.types} onChange={this.handleChange} name="types">
-                        <option value="International">International</option>
-                        <option value="National">National</option>
-                        <option value="Regional">Regional</option>
-                    </select>
-                    <input type="submit" value="Create "/>
-                </form> */}
-
+                
                 <form onSubmit={this.handleSubmit}>
                 <h3 className="m-tb-20 text-center">Add Tournament</h3>
                 <div className="row">
